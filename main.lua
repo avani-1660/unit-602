1885362546c2a18d9b367d848d78632dbadcbd03
avani-1660 <47641293+avani-1660@users.noreply.github.com>
2019-04-04 23:05:01 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+local button = display.newRect( 160, 400, 100, 60)
+
+local textBox = native.newTextField( 160, 200, 200, 60)
+textBox.id = 'text box'
+
+local output = display.newText( "", 160, 300, "Times New Roman", 20)
+output:setFillColor( 1, 1, 1)
+
+
+local function Check( event )
+
+local input = tonumber(textBox.text)
+
+	if (input >= 0) then
+		output.text = "It's positive!"
+	elseif (input <= 0) then
+		output.text = "It's negative!"
+	end
+end
+
+button:addEventListener( "touch", Check)
+
+
